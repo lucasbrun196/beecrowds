baa84ad8bb5da4116457adbe8d51d808f1fbc8f3
lucasbrun196 <luucaasbrum13@gmail.com>
2024-08-16 00:53:49 -0300

    +1

---
+++ alarme_despertador.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int hi, mi, he, me, mt;
+
+    while (true)
+    {
+        if (hi == 0 && mi == 0 && he == 0 && me == 0)
+            break;
+
+        cin >> hi >> mi >> he >> me;
+        mt = 0;
+        for (int i = hi; i < he; ++i)
+        {
+        }
+    }
+
+    return 0;
+}
